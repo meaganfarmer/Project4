e64838997d93469af4d6ce6296895aaf6a84483f
meaganfarmer <meaganfarmer@Meagans-MacBook-Pro.local>
2014-08-23 20:47:42 -0500

    finished phone number

--- Farmer_Meagan_assignment4.js
+++ Farmer_Meagan_assignment4.js
@@ -1,1 +1,72 @@
 alert("JavaScript works!");
+// string
+
+//Does a string follow a 123-456-7890 pattern like a phone number?
+
+var phone = "918-822-7535"
+var end = myString.indexOf("-");
+var prefix =myStringIndexOf("-")+1;
+var start = myString.lastIndexOf("-"+1;)
+var stopping = myString.length;
+//area code
+
+myString.Substring(0,end)
+
+//prefix
+
+myString.substring(prefix,end);
+
+//lastFour
+myString.substring(start,end)
+
+
+
+//comment
+
+//Does a string follow an aaa@bbb.ccc pattern like an email address?
+
+//comment
+
+//Is the string a URL? (Does it start with http: or https:?)
+
+//comment
+
+//Title-case a string (split into words, then uppercase the first letter of each word)
+
+//comment
+
+//Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" ? "a/b/c".
+
+//comment
+
+//number
+
+//Format a number to use a specific number of decimal places, as for money: 2.1 ? 2.10
+
+//comment
+
+//Fuzzy-match a number: is the number above or below a number within a certain percent?
+
+            //comment
+
+//Find the number of hours or days difference between two dates.
+
+            //comment
+
+//Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+
+            //comment
+
+//Array
+
+//Find the smallest value in an array that is greater than a given number
+
+            //comment
+
+//Find the total value of just the numbers in an array, even if some of the items are not numbers.
+
+            //comment
+
+//Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] ? [{a:1},{a:2},{a:3}].
+
+            //comment
