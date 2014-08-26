08c6fbc9de1776a4e62741fb47e529b105ab29e0
meaganfarmer <meaganfarmer@Meagans-MacBook-Pro.local>
2014-08-25 21:10:16 -0500

    adding conditional in phone

--- Farmer_Meagan_assignment4.js
+++ Farmer_Meagan_assignment4.js
@@ -10,12 +10,19 @@ var phoneNumber = "123-456-7891";
 var areaCode = phoneNumber.substring(0,3);
 var prefix = phoneNumber.substring(4,7);
 var lastFour = phoneNumber.substring(8,12);
-var finalNumber = areaCode + "-"+ prefix + "-" + lastFour;
+var finalNumber = areaCode + "- "+ prefix + "-" + lastFour;
+var hyphen = " - " ;
 
 
 //functions
-var makePhoneNumber = function(areaCode, prefix, lastFour){
+var makePhoneNumber = function(areaCode, hyphen, prefix, hyphen, lastFour){
+	if (finalNumber === phoneNumber )
+		console.log("awesome");
+} else {
+		console.log("Poo");
+	
+	
 };
 
 //function calls
-console.log(finalNumber);
+//console.log(finalNumber);
